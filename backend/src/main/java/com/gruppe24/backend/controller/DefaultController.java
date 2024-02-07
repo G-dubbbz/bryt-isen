144ee684664b86ccc696a5b1b6c9b829916961c4
chrisuth <chrisuth@stud.ntnu.no>
2024-02-07 16:19:01 +0100

    Update: default controller

--- backend/src/main/java/com/gruppe24/backend/controller/DefaultController.java
+++ backend/src/main/java/com/gruppe24/backend/controller/DefaultController.java
@@ -1,8 +1,17 @@
 package com.gruppe24.backend.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Default Rest Controller
+ * <p>
+ *   This controller handles the main entrypoint, which at the moment is mostly a hello world GET mapping
+ *   and a status GET mapping
+ * </p>
+ * @version 1.0
+ */
 @RestController
 public class DefaultController {
 
@@ -11,8 +20,18 @@ public class DefaultController {
         return "Hello World";
     }
 
+    /**
+     * Check the status of the server.
+     * @return HTTPS_status_code 200
+     */
+    @GetMapping("/status")
+    public ResponseEntity<?> status() {
+        return ResponseEntity.ok().build();
+    }
+
     @GetMapping("/secured")
     public String secured() {
         return "Hello, secured!";
     }
+
 }

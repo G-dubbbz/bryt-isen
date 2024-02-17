8b89de7df4fa2bc7b0fb31c1685b6c2aac652ee8
chrisuth <chrisuth@stud.ntnu.no>
2024-02-17 02:19:57 +0100

    Update: The content for the ControllerAdvice

--- backend/src/main/java/com/gruppe24/backend/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/com/gruppe24/backend/controller/GlobalExceptionHandler.java
@@ -1,2 +1,72 @@
-package com.gruppe24.backend.controller;public class GlobalExceptionHandler {
+package com.gruppe24.backend.controller;
+
+import com.gruppe24.backend.dto.ApiErrorResponse;
+import com.gruppe24.backend.exception.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+  @ExceptionHandler(UserNotFoundException.class)
+  public ResponseEntity<ApiErrorResponse> handleUserNotFoundException(UserNotFoundException e) {
+    log.error("User not found: " + e.getMessage() + ":" + e.getCause());
+    ApiErrorResponse response = new ApiErrorResponse(e.getMessage(), "USER_NOT_FOUND");
+    return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(RelationNotFoundException.class)
+  public ResponseEntity<ApiErrorResponse> handleErrorCreatingRelationException(RelationNotFoundException e) {
+    log.error("Relation not found: " + e.getMessage() + ":" + e.getCause());
+    ApiErrorResponse response = new ApiErrorResponse(e.getMessage(), "RELATION_NOT_FOUND");
+    return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(GameNotFoundException.class)
+  public ResponseEntity<ApiErrorResponse> handleGameNotFoundException(GameNotFoundException e) {
+    log.error("Game not found: " + e.getMessage() + ":" + e.getCause());
+    ApiErrorResponse response = new ApiErrorResponse(e.getMessage(), "GAME_NOT_FOUND");
+    return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(ListNotFoundException.class)
+  public ResponseEntity<ApiErrorResponse> handleListNotFoundException(ListNotFoundException e) {
+    log.error("List not found: " + e.getMessage() + ":" + e.getCause());
+    ApiErrorResponse response = new ApiErrorResponse(e.getMessage(), "LIST_NOT_FOUND");
+    return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(ErrorCreatingGameException.class)
+  public ResponseEntity<ApiErrorResponse> handleErrorCreatingGameException(ErrorCreatingGameException e) {
+    log.error("Error creating game: " + e.getMessage() + ":" + e.getCause());
+    ApiErrorResponse response = new ApiErrorResponse(e.getMessage(), "ERROR_CREATING_GAME");
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(ErrorCreatingRelationException.class)
+  public ResponseEntity<ApiErrorResponse> handleErrorCreatingRelationException(ErrorCreatingRelationException e) {
+    log.error("Error creating relation: " + e.getMessage() + ":" + e.getCause());
+    ApiErrorResponse response = new ApiErrorResponse(e.getMessage(), "ERROR_CREATING_RELATION");
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(UserExistsException.class)
+  public ResponseEntity<ApiErrorResponse> handleUserExistsException(UserExistsException e) {
+    log.error("User already exists: " + e.getMessage() + ":" + e.getCause());
+    ApiErrorResponse response = new ApiErrorResponse(e.getMessage(), "USER_ALREADY_EXISTS");
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(InvalidDtoException.class)
+  public ResponseEntity<ApiErrorResponse> handleInvalidDtoException(InvalidDtoException e) {
+    log.error("Invalid DTO: " + e.getMessage() + ":" + e.getCause());
+    ApiErrorResponse response = new ApiErrorResponse(e.getMessage(), "INVALID_DTO");
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  }
 }

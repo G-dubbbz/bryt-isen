49689b62006c0a5a82a9a3e5d72cacf0b5e90e68
Arthur Brunborg <arthur@brunborg.com>
2024-02-20 23:58:25 +0100

    Added: Test for /status

--- backend/src/test/java/com/gruppe24/backend/DefaultControllerTest.java
+++ backend/src/test/java/com/gruppe24/backend/DefaultControllerTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -21,4 +23,12 @@ public class DefaultControllerTest {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/", String.class))
             .contains("Welcome!");
     }
+
+    @Test
+    public void statusShouldReturnServiceStatus() throws Exception {
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/status", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).contains("Service is up and running");
+    }
+
 }

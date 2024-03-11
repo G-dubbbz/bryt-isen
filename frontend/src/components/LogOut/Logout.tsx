7a5bff19bfd4861fc4c7397d55477715f116cb22
chrisuth <chrisuth@stud.ntnu.no>
2024-03-11 13:29:22 +0100

    Add: Created a LogOut Component

---
+++ frontend/src/components/LogOut/Logout.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Logout = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        sessionStorage.removeItem('token');
+
+        navigate('/all');
+    }, [navigate]);
+
+    return null;
+};
+
+export default Logout;
